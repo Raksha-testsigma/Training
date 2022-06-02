a2393485d64069515cba23d547154e22d8156aa5
Raksha-testsigma <106725744+Raksha-testsigma@users.noreply.github.com>
2022-06-02 14:50:14 +0530

    Add files via upload

---
+++ bank.java
@@ -0,0 +1,25 @@
+
+class karur implements in {
+    public void interestmoney(int a){
+
+        System.out.println(a*10);
+    }
+}
+class iob implements in{
+    public void interestmoney(int b){
+        System.out.println(b*20);
+    }
+}
+class sbi implements in{
+    public void interestmoney(int c){
+        System.out.println(c*30);
+
+    }
+}
+public class bank{
+
+    public static void main(String[] args) {
+        iob intt=new iob();
+        intt.interestmoney(4);
+    }
+}
