c78f8ec8d17547df3eddbf17b8d98a4214176493
Raksha-testsigma <106725744+Raksha-testsigma@users.noreply.github.com>
2022-06-06 10:01:06 +0530

    Add files via upload

---
+++ Oops/Poly.java
@@ -0,0 +1,26 @@
+class InheritanceDemo {
+    public static int add(int i, int j) {
+        return i+j;
+    }
+}
+class Advcalc extends InheritanceDemo{   // IS - A
+    public static int sub(int i,int j){
+        return i-j;
+    }
+}
+class superAdv extends Advcalc{
+    public static int mul(int i, int j) {
+        return i*j;
+    }
+}
+public class Poly{
+    public static void main(String[] args) {
+        superAdv a = new superAdv();   //HAS - A
+        int res1=a.add(5,7);
+        int res2=a.sub(7,3);
+        int res3 = a.mul(3,3);
+        System.out.println(res1);
+        System.out.println(res2);
+        System.out.println(res3);
+    }
+}
